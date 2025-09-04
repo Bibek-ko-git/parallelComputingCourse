6547f22095c83061c0d0913840f9a8fad096ef1f
Bibek Shrestha <shresb@rpi.edu>
2025-09-04 18:16:19 -0400

    Restructuring some core part

--- Problem_Set_1/helloWorld.C
+++ Problem_Set_1/helloWorld.C
@@ -4,8 +4,8 @@ int main (int argc, char* argv[] )
 {
 	printf("Hello World!\n");
 
-	printf("argc=%d:\n ",argc);
-		for( int i=0; i<argc; i++)
+	printf("argc=%d:\n",argc);
+		for(int i=0; i<argc; i++)
 		{
 			printf("argv[%d]=%s\n",i,argv[i]);
 		}
